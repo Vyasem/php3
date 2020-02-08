c11836b86429ca1ced7086dae8864013c0ec036f
Vyacheslav Semenov <vyasem@gmail.com>
2020-02-08 15:36:43 +0300

    08.02.2020 15:29

--- protected/Controllers/Index.php
+++ protected/Controllers/Index.php
@@ -14,13 +14,4 @@ class Index
         $this->data['domain'] = $configArray['domain'];
         $this->data['title'] = 'Home page of '.$configArray['domain'];
     }
-
-    public function actionMay()
-    {
-        $configArray = $this->app->config->getData();
-        $this->data['domain'] = $configArray['domain'];
-        $this->data['title'] = 'Home page of '.$configArray['domain'];
-        $this->view->render('May.html');
-    }
-
 }
