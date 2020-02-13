04ceee9cb5e579242a1337cf1cd44b8475c11a85
Slava <vyasem@gmail.com>
2020-02-13 10:35:58 +0300

    Update up_2.sql

--- migration/up_2.sql
+++ migration/up_2.sql
@@ -2,6 +2,6 @@ CREATE TABLE role(
   id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   role varchar(255) COLLATE 'utf8_general_ci' NOT NULL
 );
-INSERT INTO users (id, name) VALUES (NULL, 'Админ');
-INSERT INTO users (id, name) VALUES (NULL, 'Пользователь');
+INSERT INTO role (id, name) VALUES (NULL, 'Админ');
+INSERT INTO role (id, name) VALUES (NULL, 'Пользователь');
 UPDATE users SET role=(SELECT id FROM role WHERE role='Админ');
