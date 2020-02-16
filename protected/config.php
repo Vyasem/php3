24372e82e1cb3fcbc6522e800a7b2917bf2fa25b
Vyacheslav Semenov <vyasem@gmail.com>
2020-02-16 15:49:52 +0000

    Docker settings

--- protected/config.php
+++ protected/config.php
@@ -5,9 +5,9 @@ return [
     "title" => 'php31.loc',
     "db" => [
         "driver" => "mysql",
-        "host" => "localhost",
-        "user" => "developer",
-        "password" => "developer",
+        "host" => "192.168.1.101:3306",
+        "user" => "slava",
+        "password" => "slava",
         "dbname" => "php3"
     ]
 ];
