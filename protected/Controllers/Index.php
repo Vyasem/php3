263aa63fb497ad4a4652ca1d0a3c479c6b50bfbc
Vyacheslav <vyasem@gmail.com>
2020-02-22 09:34:10 +0300

    22.02.2020 09:33

--- protected/Controllers/Index.php
+++ protected/Controllers/Index.php
@@ -3,16 +3,12 @@
 namespace App\Controllers;
 
 use T4\Mvc\Controller;
-use App\Models\Migration;
 
 class Index extends Controller
 {
     public function actionDefault()
     {
         $configArray = $this->app->config->getData();
-        $dbConfig = $configArray['db']->getData();
-        $migration = new Migration($dbConfig);
-        $migration->up();
         $this->data['domain'] = $configArray['domain'];
         $this->data['title'] = 'Home page of '.$configArray['domain'];
     }
